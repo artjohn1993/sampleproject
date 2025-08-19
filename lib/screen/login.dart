ab6e05ab7466f17f9b3d7bb9e73d6f7570d849c2
artjohn1993 <artjohn1993@gmail.com>
2025-08-19 16:04:07 +0800

    fix: title

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,8 +6,8 @@ class Login extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login'),),
-      body: Text('Login'),
+      appBar: AppBar(title: Text('Login Screen'),),
+      body: Text('Login '),
     );
   }
 }
