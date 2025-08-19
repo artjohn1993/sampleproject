222e249bd4bf05e83d23b66eb41e45bd0667df31
artjohn1993 <artjohn1993@gmail.com>
2025-08-19 14:31:47 +0800

    feature1

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('sample'),),
-      body: Text('hello world'),
+      body: Text('hello world 1'),
     );
   }
 }
