9bfdc155a67bd23ce20df5aaf2ac625a892815cd
artjohn1993 <artjohn1993@gmail.com>
2025-08-19 14:19:36 +0800

    fix: hello world

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('sample'),),
-      body: Text('data'),
+      body: Text('hello world'),
     );
   }
 }
